adb9b4e3513ba0c2483a340f791cb7d61b868c6a
Hossein Movahed <7905365-harry9345@users.noreply.gitlab.com>
2021-09-01 21:13:55 +0300

    adding style to form

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="shortcut icon" href="./style/favicon.png" type="image/x-icon">
   <title>Bedford Row Capital</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
@@ -16,7 +17,7 @@
     <div class="row justify-content-md-center mb-5">
       <div class="col-md-11 border p-3">
         <form class="row g-3">
-          <div class="row-auto">
+          <div class="row-auto" action="insertProduct.php" method="post">
             <label for="productId" class="col-sm-2 col-form-label">Product Id :</label>
             <input type="number" class="form-control" id="productId" placeholder="Product Id" />
           </div>
@@ -26,7 +27,7 @@
           </div>
           <div class="row-auto">
             <label for="createdDate" class="col-sm-2 col-form-label">Date :</label>
-            <input type="number" class="form-control" id="createdDate" placeholder="Created Date" />
+            <input type="text" class="form-control" id="createdDate" placeholder="Created Date" />
           </div>
 
           <div class="row-auto">
@@ -76,15 +77,15 @@
               echo '
               
               <tr>
-              <th scope="row">' . $product["ProductId"] . '</th>
-              <td>' . $product["ProductName"] . '</td>
-              <td>' . $product["NumberOfProductToBeSold"] . '</td>
-              <td>' . $product["MinimumPriceRange"] . '</td>
-              <td>' . $product["MaximumPriceRage"] . '</td>
-              <td>' . $product["CreatedDate"] . '</td>
+              <th scope="row">' . $product['ProductId'] . '</th>
+              <td>' . $product['ProductName'] . '</td>
+              <td>' . $product['NumberOfProductToBeSold'] . '</td>
+              <td>' . $product['MinimumPriceRange'] . '</td>
+              <td>' . $product['MaximumPriceRage'] . '</td>
+              <td>' . $product['CreatedDate'] . '</td>
               <td>
                 <div class="btn btn-secondary btn-sm">
-                <a href="deleteProduct.php">
+                <a href="deleteProduct.php?id=' . $product['id'] . '" >
                 <em class="far fa-trash-alt"></em>
                   </a>
                 </div>
