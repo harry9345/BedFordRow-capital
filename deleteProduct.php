00b5a79d6e8f707d39fbe1900d5ae155e96c8c88
Hossein Movahed <7905365-harry9345@users.noreply.gitlab.com>
2021-09-02 21:59:47 +0300

    fixing typo

--- deleteProduct.php
+++ deleteProduct.php
@@ -1,11 +1,12 @@
 <?php
 include "connect.php";
 $productId = $_GET["id"];
-var_dump($productId);
+
 $deleteQuery = "DELETE FROM `products` WHERE `products`.`id` = ?";
-$executeDeleteQuery = $connnect->prepare($deleteQuery);
+$executeDeleteQuery = $connect->prepare($deleteQuery);
 $executeDeleteQuery->bindValue(1, $productId);
 $deleteResult = $executeDeleteQuery->execute();
+
 if ($deleteQuery) {
     header("location: index.php");
 } else {
