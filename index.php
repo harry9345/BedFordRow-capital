8dc244e5e41228d64986482fba61b575369ef0d0
Hossein Movahed <7905365-harry9345@users.noreply.gitlab.com>
2021-09-02 21:59:08 +0300

    validation with bootstrap and js also fixing typo

--- index.php
+++ index.php
@@ -16,33 +16,60 @@
   <div class="container-fluid">
     <div class="row justify-content-md-center mb-5">
       <div class="col-md-11 border p-3">
-        <form class="row g-3">
-          <div class="row-auto" action="insertProduct.php" method="post">
+        <form class="row g-3 needs-validation" novalidate action="insertProduct.php" method="post">
+          <div class="row-auto">
             <label for="productId" class="col-sm-2 col-form-label">Product Id :</label>
-            <input type="number" class="form-control" id="productId" placeholder="Product Id" />
+            <input type="number" class="form-control" name="productId" id="productId" placeholder="Product Id" required />
+            <div class="valid-feedback">
+              Looks good!
+            </div>
+            <div class="invalid-feedback">
+              Please insert product Id .
+            </div>
           </div>
           <div class="row-auto">
             <label for="productName" class="col-sm-2 col-form-label">Product Name :</label>
-            <input type="text" class="form-control" id="productName" placeholder="Product Name" />
+            <input type="text" class="form-control" name="productName" id="productName" placeholder="Product Name" required />
+            <div class="valid-feedback">
+              Looks good!
+            </div>
+            <div class="invalid-feedback">
+              Please insert product name .
+            </div>
           </div>
-          <div class="row-auto">
-            <label for="createdDate" class="col-sm-2 col-form-label">Date :</label>
-            <input type="text" class="form-control" id="createdDate" placeholder="Created Date" />
+          <div class="row-auto ">
+            <label for="createdDate" class="col-sm-2 col-form-label"> Created Date :</label>
+            <input type="text" class="form-control" name="createdDate" id="createdDate" placeholder="DD/MM/YY or DD-MM-YY " />
+            <div class="valid-feedback">
+              Looks good!
+            </div>
           </div>
 
           <div class="row-auto">
             <label for="productSold" class="col-sm-2 col-form-label">Item To Be Sold :</label>
-            <input type="number" class="form-control" id="productSold" placeholder="Number of Product To Be Sold" />
+            <input type="number" class="form-control" name="productSold" id="productSold" placeholder="Number of Product To Be Sold" required />
+            <div class="valid-feedback">
+              Looks good!
+            </div>
+            <div class="invalid-feedback">
+              Please insert product name .
+            </div>
           </div>
           <div class="row-auto">
             <label for="minPriceRange" class="col-sm-2 col-form-label">
               Minimum Price Range :
             </label>
-            <input type="number" class="form-control" id="minPriceRange" placeholder="Min Price" />
+            <input type="number" class="form-control" name="minPriceRange" id="minPriceRange" placeholder="Min Price" />
+            <div class="valid-feedback">
+              Looks good!
+            </div>
           </div>
           <div class="row-auto">
             <label for="maxPriceRange" class="col-sm-2 col-form-label">Maximum Price Range :</label>
-            <input type="number" class="form-control" id="maxPriceRange" placeholder="Max Price" />
+            <input type="number" class="form-control" name="maxPriceRange" id="maxPriceRange" placeholder="Max Price" />
+            <div class="valid-feedback">
+              Looks good!
+            </div>
           </div>
           <div class="d-grid gap-2 col-6 mx-auto">
             <button type="submit" class="btn btn-success mb-3">Submit</button>
@@ -52,7 +79,7 @@
     </div>
     <div class="row justify-content-md-center mt-5">
       <div class="col-md-11 text-center border p-3">
-        <table class="table table-striped table-success table-hover">
+        <table class="table table-striped table-success table-hover" id="table">
           <thead>
             <tr>
               <th scope="col">Id</th>
@@ -69,7 +96,7 @@
             include "connect.php";
 
             $query = "SELECT * from products";
-            $queryResult = $connnect->prepare($query);
+            $queryResult = $connect->prepare($query);
             $queryResult->execute();
             $allData = $queryResult->fetchAll(PDO::FETCH_ASSOC);
 
@@ -77,20 +104,20 @@
               echo '
               
               <tr>
-              <th scope="row">' . $product['ProductId'] . '</th>
-              <td>' . $product['ProductName'] . '</td>
-              <td>' . $product['NumberOfProductToBeSold'] . '</td>
-              <td>' . $product['MinimumPriceRange'] . '</td>
-              <td>' . $product['MaximumPriceRage'] . '</td>
-              <td>' . $product['CreatedDate'] . '</td>
-              <td>
-                <div class="btn btn-secondary btn-sm">
-                <a href="deleteProduct.php?id=' . $product['id'] . '" >
-                <em class="far fa-trash-alt"></em>
-                  </a>
-                </div>
-              </td>
-            </tr>
+                <th scope=row>' . $product['productId'] . '</th>
+                <td>' . $product['productName'] . '</td>
+                <td>' . $product['productToBeSold'] . '</td>
+                <td>' . $product['minimumPriceRange'] . '</td>
+                <td>' . $product['maximumPriceRage'] . '</td>
+                <td>' . $product['created_at'] . '</td>
+                <td>
+                    <div class="btn btn-secondary btn-sm">
+                      <a href="deleteProduct.php?id=' . $product['id'] . '  " >
+                        <em class="far fa-trash-alt"></em>
+                      </a>
+                    </div>
+                </td>
+               </tr>
               ';
             }
             ?>
@@ -100,6 +127,7 @@
     </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+  <script src="./jsScript.js"></script>
 </body>
 
 </html>
